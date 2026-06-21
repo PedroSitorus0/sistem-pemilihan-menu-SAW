acc6ef341068e0557a327d4f6cefff1bfef8b532
Pedro Sitorus <pedrositorus0@gmail.com>
2026-06-21 23:01:57 +0700

    implement user seeder with multiple users and default password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       $users = [
+        ['nama' => 'Pedro Armando Sitorus', 'email' => 'pedrositorus0@gmail.com',],
+        ['nama'=> 'Alvi Nurhaqim Purnama', 'email' => 'alvinurhakim1997@gmail.com',],
+        ['nama'=> 'Giestiani Putri', 'email' => 'gistiii06@gmail.com',],
+        ['nama' => 'Tazqia Cantika Ananda Deka', 'email' => 'tazqiacantikaa.d@gmail.com',],
+        ['nama' => 'Tania Citra Wandini', 'email' => 'taniacitrawandini@gmail.com',],
+        ['nama' => 'Zianka Nurazizah', 'email' => 'ziankanurazizah@gmail.com'],
+       ];
+
+       
+       $defaultPassword = Hash::make('admin123'); //ini password komdigi;
+       foreach ($users as $user) {
+        User::create([
+            'nama' => $user['nama'],
+            'email' =>$user['email'],
+            'password' => $defaultPassword,
+            'role' => 'dev',
+            'nomor_induk' => null,
+        ]);
+       }
     }
 }
