347d68ad46be7e31bb2454a2790e83ae20b7e487
Pedro Sitorus <pedrositorus0@gmail.com>
2026-07-02 23:28:09 +0700

    Implement Menu management features: add CRUD operations in MenuController, update routes, and integrate DataTables for enhanced UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 
+// window.Alpine = Alpine
+// Alpine.start();
 
+//import './bootstrap';
 import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+// Import jQuery
+import $ from 'jquery';
+window.jQuery = window.$ = $;
 
+// Import DataTables Core
+//import DataTable from 'datatables.net-dt';
+//window.DataTable = DataTable;
+
+window.Alpine = Alpine;
 Alpine.start();
--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -7,20 +7,17 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.tailwindcss.css">
 
-        <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.tailwindcss.css">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -29,12 +26,15 @@
                 </header>
             @endisset
 
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
         <script src="https://cdn.datatables.net/2.0.8/js/dataTables.tailwindcss.js"></script>
+
         @stack('scripts')
     </body>
 </html>
